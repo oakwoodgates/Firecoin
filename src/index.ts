ea823c3d4a18c7dac54c7176eaa5fdd04b440f1e
oakwoodgates <wpguru4u@gmail.com>
2023-02-23 22:30:48 -0500

    add token mint functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,74 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
+import * as token from "@solana/spl-token"
+
+async function createTokenMint(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mintAuthority: web3.PublicKey,
+  freezeAuthority: web3.PublicKey,
+  decimals: number
+): Promise<web3.PublicKey> {
+
+  const tokenMint = await token.createMint(
+    connection,
+    payer,
+    mintAuthority,
+    freezeAuthority,
+    decimals
+  );
+
+  console.log(`The token mint account address is ${tokenMint}`)
+  console.log(
+      `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
+  );
+
+  return tokenMint;
+}
+
+async function createTokenAccount(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mint: web3.PublicKey,
+  owner: web3.PublicKey
+) {
+  const tokenAccount = await token.getOrCreateAssociatedTokenAccount(
+      connection,
+      payer,
+      mint,
+      owner
+  )
+  
+  console.log(
+      `Token Account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
+  )
+
+  return tokenAccount
+}
+
+async function mintTokens(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mint: web3.PublicKey,
+  destination: web3.PublicKey,
+  authority: web3.Keypair,
+  amount: number
+) {
+  const mintInfo = await token.getMint(connection, mint)
+
+  const transactionSignature = await token.mintTo(
+    connection,
+    payer,
+    mint,
+    destination,
+    authority,
+    amount * 10 ** mintInfo.decimals
+  )
+
+  console.log(
+    `Mint Token Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  )
+}
 
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
@@ -7,6 +76,7 @@ async function main() {
 
   console.log("PublicKey:", user.publicKey.toBase58())
 }
+
 
 main()
   .then(() => {
